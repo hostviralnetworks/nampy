3d30a6df728c6c9d9c35bf6301e71f010cb11e16
Brian Schmidt <brianjamesschmidt@gmail.com>
2014-05-25 16:41:59 -0400

    Updates to networkstatistics
    -Updated commenting
    -Added z_enrichment_test() to calculate significance of
    subnetworks/complexes by combining z-scores

--- nampy/statistics/networkstatistics.py
+++ nampy/statistics/networkstatistics.py
@@ -66,10 +66,8 @@ def get_pvalue_from_scores(result_dict, permutation_dict, **kwargs):
     """
     continue_flag = True
 
-    if 'verbose' in kwargs: 
-        verbose = kwargs['verbose']
-    else:
-        verbose = False
+    verbose = test_kwarg('verbose', kwargs, [False, True])
+
     
     try:
         from rpy2.robjects import r
@@ -175,7 +173,7 @@ def t_uneqvar(list_1, list_2, **kwargs):
     """ Performs a t-test without the equal variance
     assumption of Student's t.  For example, see: 
     Ruxton, G. D. (2006). 
-    The unequal variance t-test is an underused alternative to 
+    The unequal variance t-test is an alternative to 
     Student's t-test and the Mann-Whitney U test
     Behavioral Ecology, 17(4), 688–690.
 
@@ -384,10 +382,11 @@ def stouffer_z_agg(p_list, **kwargs):
 
 
 def get_mapping_list(probe_key_gene_list_values):
-    """ Remap a dict of {key: [values]}
-    So that we get the mappings.  Here we call
-    probe 1 and gene 2 in the code for simplicity
-    but this function is obviously
+    """ Useful for mapping genes to probes.
+    Remap a dict of {key: [values]}
+    So that we get the gene: probe mappings.  Here we call
+    probe "1" and gene "2" in the code for simplicity
+    since this function can be
     generic for what we are mapping
 
     Arguments:
@@ -561,7 +560,8 @@ def enrichment_test(the_present_set, the_absent_set, the_grouping_dict, **kwargs
       {the_grouping: {'p': the corrected p-value, 
        'n_present':  number in the grouping that were present,
        'n_group': the total size of the group}
-       
+
+    
     """
     from copy import deepcopy
     from scipy.stats import hypergeom
@@ -633,7 +633,263 @@ def enrichment_test(the_present_set, the_absent_set, the_grouping_dict, **kwargs
 
     return final_result_dict
 
+
+def z_enrichment_test(node_pvals_dict, the_grouping_dict, **kwargs):
+    """ Perform enrichment analysis on the groupings in the_grouping_dict, using
+     statistical aggregation based on the z-scores.  Note this is similar to
+     the statistical subnetwork scoring system used in:
+
+     Ideker, T., Ozier, O., Schwikowski, B., & Siegel, A. (2002). 
+     Discovering regulatory and signalling circuits in molecular interaction networks. 
+     Bioinformatics, 18, 233–240.
+
+     Except here we use the pre-defined groupings in the_grouping_dict rather than
+     scanning for novel subnetworks.
+
+    Arguments:
+     node_pvals_dict: must have a subdictionary of node_id:
+      'p_uncorrected': (or 'p').  Note the p-value should be from
+                      a two-tailed test for changes.
+      't': optional, to deal with two-tailedness if doing signed z aggregation)
+      p-values are assumed to result from two-tail tests and span [0 - 1].
+      'z' is optional, if 'use_type' = z then it is needed
+     the_grouping_dict: a dict of subsystem_id: [node_id_1, node_id_2, ...] 
+
+    kwargs:
+     navg_node_sample: default is 100.0.  The groupings are randomized 
+      such that each gene is sampled an
+      average of 100 times. 
+     diagnostic: [False (default), True]: if True, a list of the randomly 
+                 generated p-values will also be returned.
+     aggregation_type: options are 'signed' or 'unsigned'
+      'unsigned': the z-value ranges from 0 to + inf.  This method 
+                  picks out gross changes in subsystems and ignored 
+                  whether they are increasing or decreasing.
+      'signed': the z-value ranges from -inf to +inf.  This method picks out
+                coordinated changes in subsystems.
+
+    Returns: 
+     grouping_scores_dict with keys
+      agg_z = the aggregated z-value without background correction
+      agg_adj_z = the aggregated z-value with background correction
+      agg_p = a p-value resulting from a two-tail test for changes 
+       (e.g. near 0 is more significant) assuming the agg_adj_z is truly
+       normally distributed.
+     if diagnostic = True, random_scores_to_return is also returned.
+
+
+     
+    """
     
+    from numpy import nan, sign, mean, std, array, inf, zeros
+    from numpy.random import rand
+    from copy import deepcopy
+    from random import sample, shuffle
+    from scipy.stats import norm, randint
+    
+    diagnostic = test_kwarg('diagnostic', kwargs, [False, True])
+    aggregation_type = test_kwarg('aggregation_type', kwargs, ['unsigned', 'signed'])
 
+    if 'navg_node_sample' in kwargs: 
+        navg_node_sample = kwargs['navg_node_sample']
+    else:
+        navg_node_sample = 100.0
 
+    grouping_scores_dict = {}
         
+    node_pvals_dictl = deepcopy(node_pvals_dict)
+    random_scores_to_return = {}
+    # First get the subsystems
+    for subsystem in the_grouping_dict.keys():
+        # Enforce lower case to avoid duplication
+        subsystem = subsystem.lower()
+        if not(grouping_scores_dict.has_key(subsystem)):
+            grouping_scores_dict[subsystem] = {}
+            grouping_scores_dict[subsystem]['ind_p'] = []
+            if aggregation_type == 'signed':
+                grouping_scores_dict[subsystem]['ind_t'] = []
+            grouping_scores_dict[subsystem]['agg_p'] = nan
+            grouping_scores_dict[subsystem]['agg_z'] = nan
+            grouping_scores_dict[subsystem]['agg_adj_z'] = nan
+            grouping_scores_dict[subsystem]['ind_node'] = []
+    # Now add in pvals
+    # Give preference to uncorrected p-values since Bonferroni corrected values
+    # are truncated at 1.  Don't need a multiple testing correction
+    # since we are looking across the subsystem and correcting
+    # would potentially lose information in detected differences.
+    # We re-normalize p-values for subnetworks according to an 
+    # empirical null distribution at the end.            
+    test_node = node_pvals_dictl.keys()[0]
+    if node_pvals_dictl[test_node].has_key('p_uncorrected'):
+        p_key = 'p_uncorrected'
+    else:
+        p_key = 'p'
+
+    for subsystem in the_grouping_dict.keys():
+        subsystem_lower = subsystem.lower()
+        for the_node in the_grouping_dict[subsystem]:
+            if the_node in node_pvals_dictl.keys():
+                grouping_scores_dict[subsystem_lower]['ind_p'].append(node_pvals_dictl[the_node][p_key])
+                grouping_scores_dict[subsystem_lower]['ind_node'].append(the_node)
+            if aggregation_type == 'signed':
+                grouping_scores_dict[subsystem_lower]['ind_t'].append(node_pvals_dictl[the_node]['t'])
+
+    # Now aggregate.  Make a lookuptable of size vs p values
+    maxk = 0
+    for subsystem in grouping_scores_dict:
+        if len(grouping_scores_dict[subsystem]['ind_p']) > maxk:
+            maxk = len(grouping_scores_dict[subsystem]['ind_p'])
+    meanlookup = []
+    sdlookup = []
+    # Haven't pressure tested the window with even numbers, 
+    # but odd values make more sense anyway.
+    windowsize = 5
+    maxsize =  int(maxk + 1 + int(round((windowsize-1)/2)))
+    # It can be slow to compute system statistics, 
+    # so we need to be selective and just evaluate
+    # around the sample sizes of interest
+    k_to_evaluate = []
+    for subsystem in grouping_scores_dict:
+        k = len(grouping_scores_dict[subsystem]['ind_p'])
+        if k > 0:
+            k = list(range(max(1,(k-(windowsize-1)/2)),(k+((windowsize-1)/2)+1)))
+            k_to_evaluate.extend(deepcopy(k))
+            k_to_evaluate = list(set(k_to_evaluate))
+            k_to_evaluate.sort()
+
+    # to speed calculations pre-convert to a z-score
+    node_list = node_pvals_dictl.keys()
+    pval_list = [node_pvals_dictl[curnode][p_key] for curnode in node_list]
+
+    # Want to replace 0 or 1 pvals, 
+    # use the next nearest value
+    filter_pval_list = [x for x in pval_list if ((x > 0) & (x < 1))]
+    min_val = min(filter_pval_list)
+    max_val = min(filter_pval_list)
+    for i, x in enumerate(pval_list):
+        if x >= 1:
+            pval_list[i] = max_val
+        if x <= 0:
+            pval_list[i] = min_val
+
+    if aggregation_type == 'signed':
+        # Here, we aggregate using p-values
+        # resulting from one-tailed tests
+        # where p = 0.5 means no change and
+        # decreases in expression imply negative z
+        # when aggregating.
+        # Convert our z first then take the sign
+        # to minimize numerical issues.
+        # This method is equivalent to
+        # Stouffer's method.
+        print "Warning, verify assumptions for signed averaging, this has not been done in a while."
+        zval_list = norm.ppf(pval_list)
+        tval_list = [node_pvals_dictl[curnode]['t'] for curnode in node_list]    
+        for index, t_val in enumerate(tval_list):
+            if t_val > 0:
+                zval_list[index] = -1 * zval_list[index]
+    else:
+        # Ideker 2002 and also Patil 2005 use an
+        # undirected p-value when aggregating
+        # Z-scores for p-values - e.g. they
+        # use the "significance of the change"
+        # where more negative z corresponds to 
+		# p ~ 1 and little change.
+        zval_list = -1 * norm.ppf(pval_list)        
+    
+    for k in k_to_evaluate:
+        print('Simulating measures for subsystem number '+ str(k_to_evaluate.index(k)+1) + ' of '+ str(len(k_to_evaluate)) + '.')
+        r_z_values = []
+        # Should need more trials with small k.
+        # Rule of thumb: set size so all model 
+        # genes are sampled on the average > 7x
+        # This and windowsize = 5 seem to be result in 
+        # fairly stable statistics from trial-and-error.
+        ntrials = int(round(navg_node_sample*float(len(node_pvals_dictl))/float(k)))
+
+        # Generate random indices between 0 and nmeasures - 1, as an array of size ntrials rows and k columns
+        the_random_indices = randint.rvs(0, len(node_list), size=(ntrials, k))
+        # Faster to do this here as an array operation than call stouffer_z_agg
+        random_score_distribution = array([sum(zval_list[x]) for x in list(the_random_indices)]) /(k**0.5)
+        meanlookup.append(mean(random_score_distribution))
+        # Note SD's defined by this method are approximately size-independent
+        sdlookup.append(std(random_score_distribution))
+        if diagnostic:
+            random_score_distribution.sort()
+            random_scores_to_return[k] = random_score_distribution
+
+    # The SD as assessed here should be independent of size
+    # apply a smoothing filter here first
+    sdlookup = list(smooth(array(sdlookup), window_len=windowsize))
+
+    # The mean will be dependent on k**.5; To avoid edge effects
+    # of the window first normalize then apply the smoothing filter
+    for k in k_to_evaluate:
+        meanlookup[k_to_evaluate.index(k)] = meanlookup[k_to_evaluate.index(k)] / (k ** 0.5)
+
+    meanlookup = list(smooth(array(meanlookup), window_len=windowsize))
+
+    for k in k_to_evaluate:
+        meanlookup[k_to_evaluate.index(k)] = meanlookup[k_to_evaluate.index(k)] * (k ** 0.5)
+
+    # Re-normalize the mean before averaging    
+    for subsystem in grouping_scores_dict:
+        k = len(grouping_scores_dict[subsystem]['ind_p'])
+        if k > 0:
+            node_indices = [index for index, node in enumerate(node_list) if node in grouping_scores_dict[subsystem]['ind_node']]
+            grouping_scores_dict[subsystem]['agg_z'] = sum([zval_list[index] for index in node_indices]) / (k**0.5)
+            grouping_scores_dict[subsystem]['agg_adj_z'] = (grouping_scores_dict[subsystem]['agg_z'] - meanlookup[k_to_evaluate.index(k)] )/ sdlookup[k_to_evaluate.index(k)]
+            grouping_scores_dict[subsystem]['agg_p'] = norm.cdf(grouping_scores_dict[subsystem]['agg_adj_z'])
+            # Convert back to a two-sided p value, this is twice the one-sided value
+            if aggregation_type == 'signed':
+                if grouping_scores_dict[subsystem]['agg_p'] < .5:
+                    grouping_scores_dict[subsystem]['agg_p'] = 2*grouping_scores_dict[subsystem]['agg_p']
+                else:
+                    grouping_scores_dict[subsystem]['agg_p'] = 2*(1-grouping_scores_dict[subsystem]['agg_p'])
+            else:
+                grouping_scores_dict[subsystem]['agg_p'] = 1- grouping_scores_dict[subsystem]['agg_p']
+            if diagnostic:
+                grouping_scores_dict[subsystem]['z'] = norm.cdf(k_to_evaluate.index(k))
+
+    if not diagnostic:
+        return grouping_scores_dict
+    else:
+        return grouping_scores_dict, random_scores_to_return
+
+
+def smooth(x,window_len=11,window='flat'):
+    """Smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    Credit: modified from http://www.scipy.org/Cookbook/SignalSmooth
+
+    input:
+     x: the input signal 
+     window_len: the dimension of the smoothing window; should be an odd integer
+     window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+     flat window will produce a moving average smoothing.
+
+    output:
+     the smoothed signal
+    """
+    import numpy
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+    if window_len<3:
+        return x
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+    s=numpy.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    trimsize=((window_len-1)/2)
+    y=y[trimsize:(trimsize+len(x))]
+    return y
